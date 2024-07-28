7c5d72907371cd23b63e5f2b28ed854572edee05
Mohamed Amine <mohamedamine.manaoui@gmail.com>
2024-07-28 18:15:35 +0100

    add usleep between even number

--- philo.c
+++ philo.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmanaoui <mmanaoui@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 17:45:10 by mmanaoui          #+#    #+#             */
+/*   Updated: 2024/07/28 18:14:17 by mmanaoui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <limits.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -9,7 +21,7 @@
 // #define TIME_TO_DIE 180
 // #define TIME_TO_EAT 60
 // #define TIME_TO_SLEEP 60
-// not die
+// not work !!
 // #define NBR_PHILO 4
 // #define TIME_TO_DIE 410
 // #define TIME_TO_EAT 200
@@ -49,7 +61,7 @@ typedef struct s_help
 	struct s_philo	*philo;
 	int				deads;
 	size_t			start;
-	size_t  philo_meals;
+	size_t			philo_meals;
 	// flag for I have five argv
 	size_t			flag_meals;
 }					t_help;
@@ -70,7 +82,7 @@ size_t	get_current_time(void)
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
-int	ft_usleep(size_t milliseconds)
+int	ft_msleep(size_t milliseconds)
 {
 	size_t	start;
 
@@ -108,17 +120,10 @@ void	take_a_fork(t_philo *philo, size_t current_t)
 // 	printf("%zu %d %s", calculate_time(), philo_id, str);
 // 	pthread_mutex_unlock();
 // 	return (0);
-// }flag_meals * nbr_philo
+// }
 
 int	eating(t_philo *philo, size_t current_t)
 {
-	pthread_mutex_lock(&philo->help->mutex_monitor);
-	if (philo->help->deads)
-	{
-		pthread_mutex_unlock(&philo->help->mutex_monitor);
-		return (1);
-	}
-	pthread_mutex_unlock(&philo->help->mutex_monitor);
 	pthread_mutex_lock(&philo->help->mutex_monitor);
 	if (philo->help->deads)
 	{
@@ -141,7 +146,7 @@ int	eating(t_philo *philo, size_t current_t)
 	pthread_mutex_lock(&philo->help->mutex_monitor);
 	philo->last_time_eat = get_current_time();
 	pthread_mutex_unlock(&philo->help->mutex_monitor);
-	ft_usleep(TIME_TO_EAT);
+	ft_msleep(TIME_TO_EAT);
 	return (0);
 }
 
@@ -158,7 +163,7 @@ int	sleeping(t_philo *philo, size_t current_t)
 	printf(COLOR_GREEN "%zu %d SLEEPING\n" COLOR_RESET, current_t
 		- philo->help->start, philo->id);
 	pthread_mutex_unlock(&philo->help->mutex_data);
-	ft_usleep(TIME_TO_SLEEP);
+	ft_msleep(TIME_TO_SLEEP);
 	return (0);
 }
 
@@ -186,24 +191,36 @@ void	died(t_philo *philo, size_t current_t)
 	pthread_mutex_unlock(&philo->help->mutex_data);
 }
 
+void	dd(t_philo *philo)
+{
+	pthread_mutex_lock(philo->l_fork);
+	take_a_fork(philo, get_current_time());
+	pthread_mutex_lock(philo->r_fork);
+	take_a_fork(philo, get_current_time());
+}
+void ee(t_philo *philo)
+{
+		pthread_mutex_lock(&philo->help->mutex_monitor);
+		if (philo->help->deads)
+		{
+			pthread_mutex_unlock(&philo->help->mutex_monitor);
+			return ;
+		}
+		pthread_mutex_unlock(&philo->help->mutex_monitor);
+		
+}
+
 void	*ss(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	if (philo->id % 2 == 0)
+		usleep(60);
 	while (1)
 	{
-		pthread_mutex_lock(&philo->help->mutex_monitor);
-		if (philo->help->deads)
-		{
-			pthread_mutex_unlock(&philo->help->mutex_monitor);
-			return (NULL);
-		}
-		pthread_mutex_unlock(&philo->help->mutex_monitor);
-		pthread_mutex_lock(philo->l_fork);
-		take_a_fork(philo, get_current_time());
-		pthread_mutex_lock(philo->r_fork);
-		take_a_fork(philo, get_current_time());
+		ee(philo);
+		dd(philo);
 		if (eating(philo, get_current_time()) == 1)
 		{
 			pthread_mutex_unlock(philo->r_fork);
@@ -213,15 +230,10 @@ void	*ss(void *arg)
 		pthread_mutex_unlock(philo->r_fork);
 		pthread_mutex_unlock(philo->l_fork);
 		if (sleeping(philo, get_current_time()) == 1)
-		{
 			return (NULL);
-		}
 		if (thinking(philo, get_current_time()) == 1)
-		{
 			return (NULL);
-		}
-		// new_add_by_yassir_
-		ft_usleep((TIME_TO_DIE - (get_current_time() - philo->last_time_eat))
+		ft_msleep((TIME_TO_DIE - (get_current_time() - philo->last_time_eat))
 			/ 2);
 	}
 	return (NULL);
@@ -253,8 +265,8 @@ void	init_philo(t_help *help)
 	help->philo_meals = NBR_PHILO;
 	while (i < NBR_PHILO)
 	{
-		if (help->flag_meals)
-			help->philo[i].meals = NBR_MEALS;
+		// if (help->flag_meals)
+		// help->philo[i].meals = NBR_MEALS;
 		help->philo[i].id = i + 1;
 		help->philo[i].help = help;
 		help->philo[i].last_time_eat = get_current_time();
@@ -284,6 +296,20 @@ void	join_philo(t_help *help)
 	}
 }
 
+int	__if_max_meals__(t_help *help)
+{
+	pthread_mutex_lock(&help->mutex_monitor);
+	if (help->philo_meals == 0)
+	{
+		pthread_mutex_lock(&help->mutex_dead);
+		help->deads = 1;
+		pthread_mutex_unlock(&help->mutex_dead);
+		pthread_mutex_unlock(&help->mutex_monitor);
+		return (0);
+	}
+	pthread_mutex_unlock(&help->mutex_monitor);
+	return (1);
+}
 void	l7day(t_help *help)
 {
 	int		i;
@@ -291,19 +317,8 @@ void	l7day(t_help *help)
 
 	while (1)
 	{
-		if (help->flag_meals == 1)
-		{
-			pthread_mutex_lock(&help->mutex_monitor);
-			if (help->philo_meals == 0)
-			{
-				pthread_mutex_lock(&help->mutex_dead);
-				help->deads = 1;
-				pthread_mutex_unlock(&help->mutex_dead);
-				pthread_mutex_unlock(&help->mutex_monitor);  
-				return;	
-			}
-			pthread_mutex_unlock(&help->mutex_monitor);
-		}
+		if (help->flag_meals == 1 && __if_max_meals__(help) == 0)
+			return ;
 		i = 0;
 		while (i < NBR_PHILO)
 		{
@@ -332,7 +347,7 @@ int	main(void)
 	help->start = get_current_time();
 	help->deads = 0;
 	help->philo_meals = NBR_PHILO;
-	//for now before parsing
+	// for now before parsing
 	help->flag_meals = 1;
 	init_mutex(help);
 	init_philo(help);
